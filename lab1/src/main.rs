9f6ead9e6458a93e2b537fc6561be05e27e1e8e3
Worawut <tonmanna@gmail.com>
2019-08-29 21:18:55 +0700

    JSON OutPUT

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -1,28 +1,38 @@
-// use lab2;
-// use lab3;
-use std::cmp::Ordering;
-use std::io;
-use rand::Rng;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+// fn show_request(request: &actix_web::HttpRequest,) -> Box<Future<Item = HttpResponse, Error = Error>> {
+//     Box::new(request.body().map_err(|e| e.into()).map(move |f| {
+//         actix_web::HttpResponse::Ok()
+//             .content_type("text/plain")
+//             .body(f)
+//     }))
+// }
 
-fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    loop {
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+// pub fn index(scope: actix_web::Scope<()>) -> actix_web::Scope<()> {
+//     scope.handler("", |req: &actix_web::HttpRequest| {
+//         show_request(req)
+//     })
+// }
 
-        println!("You guessed: {}", guess);
-        
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+fn index(info: web::Path<(String, u32)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.0, info.1)
+}
+fn index2() -> impl Responder {
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(data)
+}
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(web::resource("/").to(index2)))
+        .bind("0.0.0.0:88")?
+        .run()
 }
---
+++ lab4/src/lib.rs
@@ -0,0 +1,28 @@
+// use lab2;
+// use lab3;
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+pub fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
